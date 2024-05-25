e1ad3711f55d8b4591da6491e105f2c1d44fd86d
bennettforkner <bennettforkner@users.noreply.github.com>
2024-05-25 16:27:55 -0400

    Update vite config to use flat css file again

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
