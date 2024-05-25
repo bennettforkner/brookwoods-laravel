c7e220e008b9a1c0a39728299d5b6a58bff77f15
bffork@gmail.com <admin@i-070b981e39bfb82ed.ec2.internal>
2024-05-25 01:10:09 +0000

    log git-deploy to file

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+# redirect stdout/stderr to a file
+exec >git-deploy.log 2>&1
+
 cd /home/admin/web/app.bennettforkner.com/public_html
-git pull
-npm install
-npm run build
-composer install
-php artisan migrate:fresh --seed
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    git pull
+    npm install
+    npm run build
+    composer install
+    php artisan migrate:fresh --seed
+fi
