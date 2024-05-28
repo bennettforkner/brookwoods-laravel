de22e8911e4c9d482d83b6ce11790f10e0ae34f1
bennettforkner <bennettforkner@users.noreply.github.com>
2024-05-27 21:22:18 -0400

    Fix sorting of dates

--- resources/views/pages/sessions/index.blade.php
+++ resources/views/pages/sessions/index.blade.php
@@ -1,3 +1,4 @@
+@php use \Carbon\Carbon; @endphp
 <x-layout>
     <x-color-background-section>
         <div class="py-6 sm:py-10">
@@ -10,13 +11,14 @@
             </div>
         </div>
         <div class="mx-auto max-w-2xl px-6 lg:px-8">
-				@forelse($sessions->sortByDesc('start_date') as $session)
+				@php $sessions = $sessions->sortByDesc(fn($session) => Carbon::parse($session->start_at)); @endphp
+				@forelse($sessions as $session)
 				<a href="{{ route('sessions.show', ['session' => $session]) }}">
 					<div class="bg-white rounded-lg shadow-lg mx-auto mb-4">
 						<div class="p-6">
 							<h2 class="text-xl font-semibold text-gray-900">{{ $session->name }}</h2>
 							<p class="mt-4 text-base text-gray-500">{{ $session->description }}</p>
-							<p class="mt-4 text-base text-gray-500">{{ \Carbon\Carbon::parse($session->start_at)->format('m/d') }} - {{ \Carbon\Carbon::parse($session->end_at)->format('m/d') }}, {{ \Carbon\Carbon::parse($session->end_at)->format('Y') }}</p>
+							<p class="mt-4 text-base text-gray-500">{{ Carbon::parse($session->start_at)->format('m/d') }} - {{ Carbon::parse($session->end_at)->format('m/d') }}, {{ \Carbon\Carbon::parse($session->end_at)->format('Y') }}</p>
 						</div>
 					</div>
 				</a>
